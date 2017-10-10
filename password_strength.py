21178f0e5c348db96a1a0419cddae91324526288
ProgrammingLanguageLeader <vip.shoroch@gmail.com>
2017-10-10 21:24:11 +0300

    Updated password_strength.py

--- password_strength.py
+++ password_strength.py
@@ -1,4 +1,5 @@
 import getpass
+import string
 
 
 def is_case_sensitive(password):
@@ -9,8 +10,8 @@ def is_case_sensitive(password):
 
 def contains_numerals(password):
     contains = False
-    for numeral in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-        contains = contains or (password.find(numeral) != -1)
+    for char in password:
+        contains = contains or char.isdigit()
     return contains
 
 
@@ -19,11 +20,20 @@ def contains_letters(password):
 
 
 def contains_special_chars(password):
-    special_chars_string = '" !"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~"'
-    contains = False
-    for char in special_chars_string:
-        contains = contains or password.find(char) != -1
-    return contains
+    special_chars_string = string.punctuation
+    return any(char in password for char in special_chars_string)
+
+
+def contains_numerals_and_letters(password):
+    return contains_numerals(password) and contains_letters(password)
+
+
+def contains_numerals_and_special_chars(password):
+    return contains_numerals(password) and contains_special_chars(password)
+
+
+def contains_letters_and_special_chars(password):
+    return contains_letters(password) and contains_special_chars(password)
 
 
 def is_very_long(password):
@@ -68,21 +78,31 @@ def is_in_the_blacklist(password):
 
 
 def get_password_strength(password):
-    if is_in_the_blacklist(password) or is_short(password):
-        return 1
-
     strength = 1
-    if is_very_long(password):
-        strength = 3
-    elif is_long(password):
-        strength = 2
-    strength += int(is_case_sensitive(password)) * 2
-    strength += int(contains_numerals(password) and contains_letters(password)) * 2
-    strength += int(contains_numerals(password) and contains_special_chars(password)) * 2
-    strength += int(contains_letters(password) and contains_special_chars(password)) * 2
+    bad_signs = [
+        is_in_the_blacklist,
+        is_short
+    ]
+    if any(bad_sign(password) for bad_sign in bad_signs):
+        return strength
+
+    good_signs = {
+        is_very_long: 3,
+        is_long: 2,
+        is_case_sensitive : 2,
+        contains_numerals_and_letters: 2,
+        contains_numerals_and_special_chars: 2,
+        contains_letters_and_special_chars: 2
+    }
+    for good_sign, value in good_signs.items():
+        strength += good_sign(password) * value
     return strength if strength < 10 else 10
 
 
 if __name__ == '__main__':
     password = getpass.getpass(prompt='Enter a password to analyze: ')
-    print('The password strength is ' + str(get_password_strength(password)) + ' point out of 10')
+    print(
+        'The password strength is {} point out of 10'.format(
+            get_password_strength(password)
+        )
+    )
